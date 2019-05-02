d405630ddc737a2fb1536680a0bb7df6226a67c5
Devwarlt <devwarlt@loesoft.org>
2019-05-02 11:00:36 -0300

    Update static_queue.h

--- estrutura de dados/exemplos (fila)/static_queue.h
+++ estrutura de dados/exemplos (fila)/static_queue.h
@@ -3,6 +3,8 @@
 #include "stdbool.h"
 
 #define QUEUE_LENGTH 10
+#define EMPTY_QUEUE_ELEMENT -1
+#define ENABLE_DEBUG true
 
 typedef struct {
 	int Elements[QUEUE_LENGTH], Peek, Last, Count;
@@ -29,18 +31,44 @@ bool Enqueue(Queue *queue, int element) {
 	}
 	
 	queue->Count++;
-	queue->Last += 1 % QUEUE_LENGTH;
+	queue->Last = (queue->Last + 1) % QUEUE_LENGTH;
 	queue->Elements[queue->Last] = element;
+	
+	if (ENABLE_DEBUG)
+		printf("[Enqueue Event] > Adicionando o element %i na fila pos %i.\n", element, queue->Last);
+		
+	return true;
 }
 
 int Dequeue(Queue *queue) {
 	if (IsEmpty(queue)) {
 		ThrowException("Stack Underflow", "A fila está vazia!");
-		return -1;
+		return EMPTY_QUEUE_ELEMENT;
 	}
 	
 	int element = queue->Elements[queue->Peek];
 	queue->Count--;
-	queue->Peek -= 1 & QUEUE_LENGTH;
+	queue->Peek = (queue->Peek + 1) % QUEUE_LENGTH;
+	
+	if (ENABLE_DEBUG)
+		printf("[Dequeue Event] > Removendo o element %i na fila pos %i.\n", element, queue->Peek);
+		
 	return element;
+}
+
+void AutoEnqueue(Queue *queue) {
+	printf("--- [Automatic Enqueue Event] ---\n");
+	
+	int elements[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	
+	for (int i = 0; i < sizeof(elements) / sizeof(elements[0]); i++)
+		Enqueue(queue, elements[i]);
+		
+	printf("\n");
+}
+
+void AutoDequeue(Queue *queue) {
+	printf("--- [Automatic Dequeue Event] ---\n");
+	
+	while (Dequeue(queue) != EMPTY_QUEUE_ELEMENT);
 }
