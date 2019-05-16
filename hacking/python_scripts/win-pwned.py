308cc33db78d11a4d23961e5362c7bda84bc7e24
Devwarlt <devwarlt@loesoft.org>
2019-05-16 10:36:03 -0300

    Create win-pwned.py

---
+++ hacking/python_scripts/win-pwned.py
@@ -0,0 +1,44 @@
+import os
+
+pwnedmsg = """                                                                                .. .           
+                                                      ..*(((############%#####(%%(%%%%         
+                        ,(//(#%####%%%#%##%%#*#*#%%#%%%#%%###%#%%%###%###%%%#%%##(##%%#        
+    (%#%%##%##%##%#%#####%%*#%%%%%#*(##%%%%#,#%%##%##*/.    .                      ###(        
+   .#%#%###%%%%##%%##/(.,,,.    .                                                  #%#%        
+   .##%%                                                                     .,    #%##        
+    ###%                                        ..,  ,##%%%%%#. #%%%#%%%%.  %##%   (%%#,       
+    %%%#               *(#   /%##   #%%/ %#%#(  #%%* #%%##%%%%, (####/##%#* /#%%   *%%#(       
+    (%#%,   %%%%%##%#. ##%/  (%##,  (%#, *%##%, /%%/ /%%#*      .%###  (%##  #%#   ,%#%%       
+    /###    %%%#. #%%# .#%#  (%###  %%#  *%###%.,%## .###*  .    #%##  %###. #%#    %%%#       
+     (/%/   (#%#  ,#%#. ##%* /#%(# .##%  .%###/#((##  %#####%%   #%%#  ##%#.  #*    #%%%       
+     %%%#   /##%#*(#%%  ,%##./#%#####%#   ##(##(**#%  %%%#, .    %#%#  #%##,        /#%%       
+     %%%#   *#%(%####    *#%#(,  %%####   %##* #,##%. #/##       (###. *###, #%##   ,###.      
+     #/#%    %###         %#%##  /####/   #### .###%/ ###%#%#%## (%##%%%%%#  /%%#   .#%#/      
+     #%#%,   %%%#         (%%##   ###%,   ##%#  *##%% ,%%###%%%% .%%#%#(,           .%%%#      
+     *%%#/   #%%#.         #%%#   (%%%     (/,                                       #%##      
+     .%###   ,(,/                                                                    #%%%      
+      #*%#                                                                         ..##%#,     
+      ####                                             ...*(%#%#%%%%%%%#%%##%%###%#%#%%%#(     
+      *###                ,,,./##(((#%%%%##%%##%%%#####%##%%#%%/#%##%#%#####%%%#%#/,.,         
+      ####%%###%%#####(#%%*#/%#%%%%%%%#%%#*#%%%#%%%###/,,*,                                    
+       %%%#/.#%%%#%##%(#####((*.                                                               \n\n\n"""
+
+print(pwnedmsg) #display pwned message
+
+command = ''
+
+os.system('start /wait cmd /c {command}')
+
+# https://www.exploit-db.com/
+# https://www.offensive-security.com/metasploit-unleashed/using-exploits/
+# https://www.offensive-security.com/metasploit-unleashed/custom-scripting/
+# https://www.tutorialspoint.com/python/pdf/python_quick_guide.pdf
+# https://www.youtube.com/watch?v=OYsMNtCsBHQ
+# https://stackoverflow.com/questions/22163422/using-python-to-open-a-shell-environment-run-a-command-and-exit-environment
+# https://stackoverflow.com/questions/11615455/python-start-new-command-prompt-on-windows-and-wait-for-it-finish-exit
+# http://www.cs.bu.edu/courses/cs108/guides/runpython.html
+# https://manytools.org/hacker-tools/convert-images-to-ascii-art
+# https://stackoverflow.com/questions/5486725/how-to-execute-a-command-prompt-command-from-python
+# https://stackoverflow.com/questions/14940769/how-make-python-program-run-hidden-mode-in-exe-format
+# https://blog.ropnop.com/transferring-files-from-kali-to-windows/
+# https://security.stackexchange.com/questions/160601/difference-between-windows-shell-reverse-tcp-and-windows-shell-reverse-tcp-e
