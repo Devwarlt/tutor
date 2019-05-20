3c270e662036a869be91ef8f2b60fcd7cd5383cd
d3v <devwarlt@loesoft.org>
2019-05-20 09:16:21 -0300

    Create exception.h

---
+++ estrutura de dados/projetos/exception.h
@@ -0,0 +1,10 @@
+/*
+  Try / Catch block exceptions for C language, this feature still work in progress and aim for academic & testing purposes.
+  
+  -- Lambda expression in C
+  [1]. https://blog.noctua-software.com/c-lambda.html
+
+  -- Try / catch block in C
+  [1]. https://stackoverflow.com/questions/10586003/try-catch-statements-in-c
+  [2]. http://www.di.unipi.it/~nids/docs/longjump_try_trow_catch.html
+*/
