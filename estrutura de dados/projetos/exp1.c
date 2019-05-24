b738edce5a43f8ce3346d18e7799fd1b79a01e89
Devwarlt <devwarlt@loesoft.org>
2019-05-23 22:44:01 -0300

    More progress along C exception handling

--- estrutura de dados/projetos/exp1.c
+++ estrutura de dados/projetos/exp1.c
@@ -1,24 +1,36 @@
+// https://www.tutorialspoint.com/cprogramming/c_error_handling.htm
+// https://devdocs.io/c/error/errno
+// https://devdocs.io/c/string/byte/strerror
+// https://devdocs.io/c/io/perror
+// https://devdocs.io/c/error/ignore_handler_s
+
 #include "stdio.h"
 #include "stdlib.h"
-#include "setjmp.h"
+#include "errno.h"
+#include "string.h"
 
-void ExceptionSample(void) {
-	jmp_buf buf;
+char Method(void) {
+	#ifdef __STDC_LIB_EXT1__
+		set_constraint_handler_s(ignore_handler_s);
+		set_constraint_handler_s(abort_handler_s);
+	#endif
 	
-	if (setjmp(buf) == 0) {
-		if (1) {
-			
-		} else {
-			
-		}
-	}
-	else {
-		longjmp(buf, 1);
-		
-		printf("An error occurred!");
-	}
+	char* text;
+	
+	printf("Normal string: ");
+	printf("%s.\n", &"text");
+	
+	system("pause");
+	
+	printf("Null string: ");
+	printf("%s.\n\n", text);
+	
+	return (char)EXIT_SUCCESS;
 }
 
 int main(void) {
+	if (Method() == NULL)
+		return EXIT_FAILURE;
 	
+	return EXIT_SUCCESS;
 }
